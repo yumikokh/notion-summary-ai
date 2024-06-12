859e83f3b5b80e20b9ec49893b0fe940b82f23bf
Yumiko Kokubu <ymymyummy@gmail.com>
2024-06-12 22:58:08 +0900

    feat: 引数でプロンプトを入力

--- main.ts
+++ main.ts
@@ -4,9 +4,15 @@ const openai = new OpenAPI({
   apiKey: process.env["OPENAI_API_KEY"],
 });
 
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("プロンプトを入力してください");
+  process.exit(1);
+}
+
 const main = async () => {
-  const content =
-    "This package is compatible with server-side V8 contexts such as Node.js, Deno, and Cloudflare Workers.を訳して";
+  const content = args[0];
   const stream = await openai.chat.completions.create({
     model: "gpt-4",
     messages: [{ role: "user", content }],
