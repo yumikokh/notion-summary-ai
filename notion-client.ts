e27ad5ddb315fa2ad6a3ee8d92b2a2dde3a44d35
Yumiko Kokubu <ymymyummy@gmail.com>
2024-06-14 20:15:30 +0900

    関数に分ける

--- notion-client.ts
+++ notion-client.ts
@@ -4,37 +4,45 @@ const notionApi = new Client({
   auth: process.env["NOTION_API_TOKEN"],
 });
 
-export const main = async () => {
+const fetchPagesFromDatabase = async (database_id: string) => {
   const response = await notionApi.databases.query({
-    database_id: process.env["NOTION_DB_ID"] ?? "",
+    database_id,
     filter: {
       and: [
         {
           property: "Date",
           date: {
-            after: "2024-05-01",
+            on_or_after: "2024-05-01",
           },
         },
         {
           property: "Date",
           date: {
-            before: "2024-05-03",
+            before: "2024-05-02",
           },
         },
       ],
     },
   });
-  const pageIds = response.results.map(({ id }) => id);
-  const pages = await Promise.all(
-    pageIds.map(
-      async (pageId) =>
-        await notionApi.blocks.children.list({ block_id: pageId })
-    )
+  console.log(response.results);
+  return response.results;
+};
+
+const fetchBlocksFromPage = async (pageId: string) => {
+  const response = await notionApi.blocks.children.list({ block_id: pageId });
+  return response.results;
+};
+
+export const main = async () => {
+  const pages = await fetchPagesFromDatabase(process.env["NOTION_DB_ID"] ?? "");
+  const pageIds = pages.map((page) => page.id);
+  const texts = await Promise.all(
+    pageIds.map(async (pageId) => {
+      const blocks = await fetchBlocksFromPage(pageId);
+      return blocks;
+    })
   );
-  // console.log(response);
-  console.log(
-    pages.map((blockList) => blockList.results.flat().map((result) => result))
-  );
+  console.log(texts);
 };
 
 main();
