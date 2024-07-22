2766bd4a20b3cd757f3a42faf3c177dda7324cc6
Yumiko Kokubu <ymymyummy@gmail.com>
2024-07-22 15:00:10 +0900

    feat: propertiesを動的に取得する

--- src/helpers/notion.ts
+++ src/helpers/notion.ts
@@ -1,5 +1,6 @@
 import {
   BlockObjectResponse,
+  PageObjectResponse,
   RichTextItemResponse,
 } from "@notionhq/client/build/src/api-endpoints";
 
@@ -17,7 +18,9 @@ export const parseRichText = (
   return richTexts.map((text) => text.plain_text).join(option.separator);
 };
 
-export const retrievePlainTextByBlock = (block: BlockObjectResponse) => {
+export const retrievePlainTextByBlock = (
+  block: BlockObjectResponse
+): string => {
   return match(block)
     .with(
       { type: "paragraph", paragraph: { rich_text: P.select() } },
@@ -66,7 +69,9 @@ export const retrievePlainTextByBlock = (block: BlockObjectResponse) => {
       parseRichText(richText, { prefix: ">" })
     )
     .with({ type: "table_row", table_row: { cells: P.select() } }, (cell) =>
-      cell.map((richText) => parseRichText(richText, { separator: "|" }))
+      cell
+        .map((richText) => parseRichText(richText, { separator: "|" }))
+        .join("/n")
     )
     .with(
       { type: "synced_block" },
@@ -93,4 +98,88 @@ export const retrievePlainTextByBlock = (block: BlockObjectResponse) => {
       () => ""
     )
     .exhaustive();
+};
+
+type Property = PageObjectResponse["properties"][string];
+type WithoutId<T> = T extends { id: string } ? Omit<T, "id"> : T;
+
+const retrievePropertyValue = (
+  property: WithoutId<Property>
+): string | number | null =>
+  match(property)
+    .with({ type: "title" }, (value) => {
+      return parseRichText(value.title);
+    })
+    .with({ type: "number" }, (value) => {
+      if (value.number === null) return null;
+      return value.number;
+    })
+    .with({ type: "url" }, (value) => {
+      if (value.url === null) return null;
+      return value.url;
+    })
+    .with({ type: "select" }, (value) => {
+      if (value.select === null) return null;
+      return value.select.name;
+    })
+    .with({ type: "multi_select" }, (value) => {
+      if (value.multi_select.length === 0) return null;
+      return value.multi_select.map((v) => v.name).join(", ");
+    })
+    .with({ type: "status" }, (value) => {
+      if (value.status === null) return null;
+      return value.status.name;
+    })
+    .with({ type: "date" }, (value) => {
+      if (value.date === null) return null;
+      return value.date.start;
+    })
+    .with({ type: "email" }, (value) => {
+      if (value.email === null) return null;
+      return value.email;
+    })
+    .with({ type: "phone_number" }, (value) => {
+      if (value.phone_number === null) return null;
+      return value.phone_number;
+    })
+    .with({ type: "checkbox" }, (value) => {
+      return value.checkbox ? "Yes" : "No";
+    })
+    .with({ type: "rich_text" }, (value) => {
+      return parseRichText(value.rich_text);
+    })
+    .with({ type: "rollup" }, (value) => {
+      if (value.rollup.type === "array") {
+        return value.rollup.array
+          .map((v) => retrievePropertyValue(v))
+          .filter((v) => v !== null)
+          .join(", ");
+      }
+      return retrievePropertyValue(value.rollup);
+    })
+    .with(
+      { type: "files" },
+      { type: "created_by" },
+      { type: "created_time" },
+      { type: "last_edited_by" },
+      { type: "last_edited_time" },
+      { type: "formula" },
+      { type: "button" },
+      { type: "unique_id" },
+      { type: "verification" },
+      { type: "people" },
+      { type: "relation" },
+      () => null
+    )
+    .exhaustive();
+
+export const retrieveProperties = (
+  properties: PageObjectResponse["properties"]
+): string[] => {
+  return Object.entries(properties)
+    .map(([key, property]) => {
+      const value = retrievePropertyValue(property);
+      return value !== null ? `${key}: ${value}` : null;
+    })
+    .filter((v) => v !== null);
 };
--- src/notion-client.ts
+++ src/notion-client.ts
@@ -1,13 +1,5 @@
 import { Client } from "@notionhq/client";
-import {
-  BlockObjectResponse,
-  CheckboxPropertyItemObjectResponse,
-  DatePropertyItemObjectResponse,
-  PageObjectResponse,
-  RichTextItemResponse,
-  SelectPropertyItemObjectResponse,
-} from "@notionhq/client/build/src/api-endpoints";
-import { parseRichText, retrievePlainTextByBlock } from "./helpers/notion";
+import { retrievePlainTextByBlock, retrieveProperties } from "./helpers/notion";
 import { formatDate } from "./helpers/date";
 
 const notionApi = new Client({
@@ -66,56 +58,49 @@ const fetchBlocksByPage = async (pageId: string) => {
   return response.results;
 };
 
-type JournalProperties = {
-  Date: DatePropertyItemObjectResponse;
-  Feeling: SelectPropertyItemObjectResponse;
-  English: CheckboxPropertyItemObjectResponse;
-  Exercise: CheckboxPropertyItemObjectResponse;
-  Notes: {
-    id: string;
-    type: "rich_text";
-    rich_text: RichTextItemResponse[];
-  }; // 実際のデータと型が異なるので自前で定義
-};
-
 /**
  * 指定した期間のページのテキストを取得する
  * @param from 開始日
  * @param to 終了日
  */
-const fetchPagesText = async (from: Date, to: Date) => {
+const fetchPagesContents = async (from: Date, to: Date) => {
   const pages = await fetchPagesByDatabase(
     process.env["NOTION_DB_ID"] ?? "",
     from,
     to
   );
-  const texts = await Promise.all(
-    pages.map(async (_page) => {
-      const page = _page as PageObjectResponse;
-      const properties = page.properties as JournalProperties;
-      const date = properties.Date.date?.start || "";
-      const feeling = properties.Feeling.select?.name || "";
-      const english = properties.English.checkbox || false;
-      const exercise = properties.Exercise.checkbox || false;
-      const notes = parseRichText(properties.Notes.rich_text);
-      const blocks = await fetchBlocksByPage(page.id);
-      const contents = blocks
-        .map((block) => retrievePlainTextByBlock(block as BlockObjectResponse))
-        .join("\n");
 
-      return `
-日付: ${date}
-気分: ${feeling}
-英語した: ${english ? "Yes" : "No"}
-運動した: ${exercise ? "Yes" : "No"}
-ノート: ${notes}
-本文:
-${contents}
--------------------------`;
+  const texts = await Promise.all(
+    pages.flatMap(async (page) => {
+      if (page.object !== "page") {
+        return [];
+      }
+
+      let text: string = "";
+
+      if ("properties" in page) {
+        text += retrieveProperties(page.properties).join("\n");
+      }
+
+      const blocks = await fetchBlocksByPage(page.id);
+
+      text += "\nContents:\n";
+      text += blocks
+        .map((block) => {
+          if ("type" in block) {
+            return retrievePlainTextByBlock(block);
+          }
+          return null;
+        })
+        .filter((text) => text !== null)
+        .join("\n");
+      text += "\n-------------------------";
+
+      return text;
     })
   );
-  // console.log(texts.join("\n"));
+
   return texts.join("\n");
 };
 
-export { fetchPagesText };
+export { fetchPagesContents };
