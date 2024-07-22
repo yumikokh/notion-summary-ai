a952d62b4f446a94118bec813b917a778379e9d2
Yumiko Kokubu <ymymyummy@gmail.com>
2024-07-22 15:00:34 +0900

    feat: openapiを実行しないためのoptionを追加

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import OpenAPI from "openai";
-import { fetchPagesText } from "./notion-client";
+import { fetchPagesContents } from "./notion-client";
 import { Command } from "commander";
 import { endMonth, startMonth } from "./helpers/date";
 
@@ -14,20 +14,32 @@ program
     "-t, --to <toDate>",
     "終了日 (例: 2024-07-31), 省略時は開始日の月末まで"
   )
-  .option("-p, --prompt <prompt>", "プロンプト文");
+  .option("-p, --prompt <prompt>", "プロンプト文")
+  .option(
+    "-d, --dry-run",
+    "実行せずにプロンプトとNotionから取得したコンテンツを表示する"
+  );
 
 program.parse(process.argv);
 
-const { from: fromDate, to: toDate, prompt } = program.opts();
+const { from: fromDate, to: toDate, prompt, dryRun } = program.opts();
 
 const main = async () => {
   const promptText = prompt || "要約してください。";
   const from = fromDate ? new Date(fromDate) : startMonth(new Date());
   const to = toDate ? new Date(toDate) : endMonth(from);
-  const notionText = await fetchPagesText(from, to);
+  const notionContents = await fetchPagesContents(from, to);
+
   const content = `
-${promptText}
-${notionText}`;
+  プロンプト: ${promptText}
+  ----------
+  
+  ${notionContents}`;
+
+  if (dryRun) {
+    console.log(content);
+    return;
+  }
 
   const stream = await openai.chat.completions.create({
     model: "gpt-4",
